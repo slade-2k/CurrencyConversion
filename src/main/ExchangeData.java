1ca95ef6d32c99121384bf758baa34e1ec1a1000
Ingo Hotischeck <i.hotischeck@iks-gmbh.com>
2015-11-06 15:20:58 +0100

    readFile- und readURL-Methode zusammengefasst zu readSource. Falls Data.txt nicht vorhanden ist wird diese gefetcht. Aktueller Unixtimestamp wird mit dem der Data.txt verglichen, falls dieser mehr als einen Tag (24h) abweicht, werden aktuelle Konversions Raten gefetcht

--- src/main/ExchangeData.java
+++ src/main/ExchangeData.java
@@ -2,7 +2,7 @@ package main;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileInputStream;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -12,68 +12,85 @@ import java.util.HashMap;
 
 import com.google.gson.Gson;
 
-
 public class ExchangeData {
-	
+
 	private GsonData gsonData;
 	private String[] countryCodes;
 	private HashMap<String, Double> data;
 	private String fileString = "data.txt";
-	
-	public ExchangeData(){
+	private String urlString = "https://openexchangerates.org/api/latest.json?app_id=b55158a06b514c378cca1bf1274f0c52";
+
+	public ExchangeData() {
 		gsonData = new GsonData();
 	}
-	
-	public void checkData(){
-		//this.setNewExchangeRates();  //Bedingungen setzen, hier wird entschieden ob neue Daten geladen werden oder die alten aktuell genug sind
-		this.setExchangeRates();
+
+	public void checkData() {
+		this.getExchangeRates();
+		long now = System.currentTimeMillis() / 1000L;
+		long lastFetch = Long.parseLong(gsonData.timestamp);
+		if(now - lastFetch > 86400){
+			this.setNewExchangeRates();
+		}
+
 	}
-	
-	public void setExchangeData(String json){     //Erstellt neue Datei aus den gezogenen Daten
+
+	public void setExchangeData(String json) { // Erstellt neue Datei aus den
+												// gezogenen Daten
 		try {
 			FileWriter fWriter = new FileWriter(fileString);
 			BufferedWriter data = new BufferedWriter(fWriter);
 			data.write(json);
 			data.close();
+			this.getExchangeRates();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public void setExchangeRates(){
+
+	private void getExchangeRates() {
+		File file = new File("data.txt");
+		if(!file.exists()) {
+			this.setNewExchangeRates();
+		}
 		Gson gson = new Gson();
-		gsonData = gson.fromJson(readURL(fileString), GsonData.class);
+		gsonData = gson.fromJson(readSource(fileString), GsonData.class);
 		data = gsonData.rates;
 		this.setCountryCodes();
 	}
-	
+
 	public void setNewExchangeRates() { // Holt sich die neuen Daten von
 										// Openexchangerates.org und speichert
 										// den String der durch readURL()
 										// erstellt wurde in die Hashmap data
-		String url = "https://openexchangerates.org/api/latest.json?app_id=b55158a06b514c378cca1bf1274f0c52";
-		String json = readURL(url);
+		String json = readSource(urlString);
 		this.setExchangeData(json);
 	}
-	
-	public Double getExchangeData(String key){
+
+	public Double getExchangeData(String key) {
 		return data.get(key);
 	}
-	
-	public void setCountryCodes(){									//Liest die Abkürzungen aus dem KeySet der erstellten Hashmap
+
+	public void setCountryCodes() { // Liest die Abkürzungen aus dem KeySet der
+									// erstellten Hashmap
 		String[] countryCodes = gsonData.rates.keySet().toArray(new String[gsonData.rates.keySet().size()]);
 		this.countryCodes = countryCodes;
 	}
-	
-	public String[] getCountryCodes(){
+
+	public String[] getCountryCodes() {
 		return this.countryCodes;
 	}
-	
-	private String readFile(String fileString){
+
+	private String readSource(String source) {
 		BufferedReader reader = null;
 		try {
-			FileReader fReader = new FileReader(fileString);
-			reader = new BufferedReader(fReader);
+			if (source.equals(this.fileString)) {
+				FileReader fReader = new FileReader(source);
+				reader = new BufferedReader(fReader);
+			} else {
+				URL url = new URL(source);
+				reader = new BufferedReader(new InputStreamReader(url.openStream()));
+			}
+
 			StringBuffer buffer = new StringBuffer();
 			char[] chars = new char[8192];
 			int read;
@@ -88,28 +105,9 @@ public class ExchangeData {
 		}
 		return null;
 	}
-	
-	private String readURL(String urlString) {						//Liest die Json Datei aus und speichert die Daten in einem String
-		BufferedReader reader = null;
-		try {
-			URL url = new URL(urlString);
-			reader = new BufferedReader(new InputStreamReader(url.openStream()));
-			StringBuffer buffer = new StringBuffer();
-			char[] chars = new char[8192];
-			int read;
-			while ((read = reader.read(chars)) != -1) {
-				buffer.append(chars, 0, read);
-				return buffer.toString();
-			}
-		} catch (Exception e) {
 
-		} finally {
-			closeReader(reader);
-		}
-		return null;
-	}
-
-	private void closeReader(BufferedReader reader) {						//Schließt den BufferedReader 
+	private void closeReader(BufferedReader reader) { 	// Schließt den
+														// BufferedReader
 		if (reader != null) {
 			try {
 				reader.close();
